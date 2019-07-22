6cbffac2458d90bbfc51a0a75e6f3cc1b21fa98b
Sam Pawson <samuel.pawson@gmail.com>
2019-07-22 19:04:45 +0100

    added basic g maps functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,9 @@ var navBtn = $("#navbar-responsive-btn")
 var width = screen.width;
 var height = screen.height;
 
+function initMap() {
+  var selby = {lat: 53.786196, lng: -1.075116};
+  var map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 17, center: selby});
+  var marker = new google.maps.Marker({position: selby, map: map});
 }
