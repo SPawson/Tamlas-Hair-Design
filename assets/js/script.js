69a77e01088e8194be47904ce61e82d560ebaa9f
Sam Pawson <samuel.pawson@gmail.com>
2019-07-23 20:19:14 +0100

    refines services styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,31 @@ $(document).ready(function() {
     interval: 2000
   })
 
+
+  var hairExtBtn = $('#hair-ext-btn');
+  var nailsBtn = $('#nails-btn');
+  var mbBtn = $('#mb-btn');
+  var makeupBtn = $('#makeup-btn');
+
+//Toggle events to display info on service cards
+  $('.description').hide();
+
+  hairExtBtn.on('click',function(){
+    $('#hair-txt').toggle('slow');
+  });
+
+  nailsBtn.on('click',function(){
+    $('#nails-txt').toggle('slow');
+  });
+  mbBtn.on('click',function(){
+    $('#mb-text').toggle('slow');
+  });
+  makeupBtn.on('click',function(){
+    $('#makeup-txt').toggle('slow');
+  });
+
+
+
 })
 
 var navBtn = $("#navbar-responsive-btn")
