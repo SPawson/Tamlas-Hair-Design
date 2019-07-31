1472370afee40996b12a674f0d333fea999729f1
Sam Pawson <samuel.pawson@gmail.com>
2019-07-31 20:09:21 +0100

    updated nav colour and third section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-  $('.carousel').carousel({
-    interval: 6000
-  })
 
-//variables
+
+  //variables
   var hairExtBtn = $('#hair-ext-btn');
   var nailsBtn = $('#nails-btn');
   var mbBtn = $('#mb-btn');
@@ -11,40 +9,48 @@ $(document).ready(function() {
   var ghds = $('#ghd-btn');
   var shampoo = $('#shampoo-btn');
   var hairProducts = $('#hairwax-btn');
+  var callout = $('#callout-items');
 
-//Toggle events to display info on service cards
+//Make carousels cycle slowly between slides
+  $('.carousel').carousel({
+    interval: 6000
+  })
+
+  //Toggle events to display info on service cards
   $('.description').hide();
 
-//Services page buttons/text
-  hairExtBtn.on('click',function(){
+  //Services page buttons/text
+  hairExtBtn.on('click', function() {
     $('#hair-txt').toggle('slow');
     btnText(hairExtBtn);
   });
 
-  nailsBtn.on('click',function(){
+  nailsBtn.on('click', function() {
     $('#nails-txt').toggle('slow');
     btnText(nailsBtn);
   });
-  mbBtn.on('click',function(){
+  mbBtn.on('click', function() {
     $('#mb-text').toggle('slow');
     btnText(mbBtn);
   });
-  makeupBtn.on('click',function(){
+  makeupBtn.on('click', function() {
     $('#makeup-txt').toggle('slow');
     btnText(makeupBtn);
   });
 
-//Products page buttons/text
-ghds.on('click',function(){
-  $('#ghd-txt').toggle('slow');
-});
+  //Products page buttons/text
+  ghds.on('click', function() {
+    $('#ghd-txt').toggle('slow');
+  });
 
-shampoo.on('click',function(){
-  $('#shampoo-txt').toggle('slow');
-});
-hairProducts.on('click',function(){
-  $('#wax-txt').toggle('slow');
-});
+  shampoo.on('click', function() {
+    $('#shampoo-txt').toggle('slow');
+  });
+  hairProducts.on('click', function() {
+    $('#wax-txt').toggle('slow');
+  });
+
+  callout.fadeToggle(2000);
 
 })
 
@@ -53,16 +59,25 @@ var width = screen.width;
 var height = screen.height;
 
 function initMap() {
-  var selby = {lat: 53.786196, lng: -1.075116};
+  var selby = {
+    lat: 53.786196,
+    lng: -1.075116
+  };
   var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 17, center: selby});
-  var marker = new google.maps.Marker({position: selby, map: map});
+    document.getElementById('map'), {
+      zoom: 17,
+      center: selby
+    });
+  var marker = new google.maps.Marker({
+    position: selby,
+    map: map
+  });
 }
 
-function btnText(btn){
-  if(btn.text() == "Read More"){
+function btnText(btn) {
+  if (btn.text() == "Read More") {
     btn.text("Read Less");
-  }else{
+  } else {
     btn.text("Read More");
   }
 }
